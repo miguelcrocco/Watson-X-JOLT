4b2dcfd06b7f0f7ea2e465cff1edb2788399abf8
miguelcrocco <miguel.crocco@ibm.com>
2025-08-27 11:30:11 -0300

    Delete product-assistant directory

--- product-assistant/remove.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -x
-
-orchestrate env activate wxo-main
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-orchestrate knowledge-bases remove -n db2-knowledge
-orchestrate agents remove -n ProductAssistant -k native
-orchestrate agents remove -n db2_agent -k native
